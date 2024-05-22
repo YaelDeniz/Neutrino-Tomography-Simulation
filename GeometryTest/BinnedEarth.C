bab3836a72591e0e7eec56167683168e2baa3ec5
Yael Deniz <deni5876@vandals.uidaho.edu>
2024-05-22 16:14:04 -0700

    Update BinnedEarth.C

--- GeometryTest/BinnedEarth.C
+++ GeometryTest/BinnedEarth.C
@@ -395,8 +395,11 @@ void BinnedEarth()
   {
     //TestL = TestL + EarthPath[i][0];
 
-    std::cout << i << " "  << TestL << " " << rPREM - TestL << " "  << EarthPath[i][0] << " " << EarthPath[i][1] << " " << EarthPath[i][2] << " " << EarthPath[i][3] << " " << LabelLayer(EarthPath[i][3]) <<  " | " << LabelLayer(abs (rPREM - TestL) ) << std::endl;
-    TestL = TestL + EarthPath[i][0];
+    //std::cout << i << " "  << TestL << " " << rPREM - TestL << " "  << EarthPath[i][0] << " " << EarthPath[i][1] << " " << EarthPath[i][2] << " " << EarthPath[i][3] << " " << LabelLayer(EarthPath[i][3]) <<  " | " << LabelLayer(abs (rPREM - TestL) ) << std::endl;
+    //TestL = TestL + EarthPath[i][0];
+
+    std::cout << EarthPath[i][0] << " " << EarthPath[i][1] << " " << EarthPath[i][2] << " " << std::endl;
+    //TestL = TestL + EarthPath[i][0];
     
   }
 
