65449e185d6eb78e41209570e25499f7b5d0be6c
Yael Deniz <deni5876@vandals.uidaho.edu>
2024-05-24 14:59:37 -0700

    Matlab Good Plot

--- SimulationResults/TrueEventsResults/TrueEventPlots.m
+++ SimulationResults/TrueEventsResults/TrueEventPlots.m
@@ -11,17 +11,19 @@ data(:,1:2) = event_diff(:,1:2);
 data(:,3)  = event_diff(:,5); %%Difference in events
 
 %create surface plot
-zen = unique(data(:,1));
+zen = unique(data(:,1)); %Xaxis
 
 cz = cosd(zen)
-e   = unique(data(:,2));
+
+e   = unique(data(:,2)); %Yaxis
+
 dn_ij= data(:,3);
 
-[E,Zen] = meshgrid(e,zen);
+[Zen,E] = meshgrid(zen,e);
 
-[E,CZ] = meshgrid(e,cz);
+%[E,CZ] = meshgrid(e,cz);
 
-dN = reshape(dn_ij,length(e),length(cz));
+dN = reshape(dn_ij,length(zen),length(e));
 
 
 %Plot events
@@ -29,7 +31,7 @@ dN = reshape(dn_ij,length(e),length(cz));
 %%imagesc(n_ij)
 figure('Renderer', 'painters', 'Position', [10 10 1000 800])
 %datatrue=pcolor(Et,Etat, dNt');
-datatrue=pcolor(Zen,E, dN');
+datatrue=pcolor(Zen,cosd(E), dN);
 set(gca,'FontSize',20, 'FontName', 'Courier')
 set(datatrue,'edgecolor','none')
 title( 'Perfect resolution' ,'FontSize',30);
@@ -42,10 +44,10 @@ ylabel('E_{\nu}[GeV]','FontSize',30)
 
 hold on 
 
-topl = xline(138.8,'--','Top of LLVP','LineWidth',2.5) %Upper Mantle
+topl = xline(cosd(138.8),'--','Top of LLVP','LineWidth',2.5) %Upper Mantle
 topl.FontSize=25;
 
-cmbl = xline(146.8,'--','CMB','LineWidth',2.5) %CMB
+cmbl = xline(cosd(146.8),'--','CMB','LineWidth',2.5) %CMB
 cmbl.FontSize=25;
 
 hold off
