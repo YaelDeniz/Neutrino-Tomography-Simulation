2817942a66f391b9bc438e2271123dd3a95baa88
Yael Deniz <deni5876@vandals.uidaho.edu>
2024-05-29 13:39:39 -0700

    Bilinear Interpolation test

---
+++ FluxTest/FluxTest.C
@@ -0,0 +1,28 @@
+void FluxTest()
+{
+
+    TCanvas *c1 = new TCanvas();
+    TH2D *hist = new TH2F("hist","Histogram",100,-1,1,100,-1,1);
+    hist->SetStats(0);
+
+    TRandom *rand =  new TRandom(10);
+
+    for (int i = 0; i < 1e6 ; i++)
+    {
+
+        double x = rand->Gaus();
+        double y = rand->Gaus();
+
+        hist->Fill(x,y);
+
+    }
+
+    hist->Draw();
+
+    hist->Draw("COLZ");
+    c1->Print("./Hist2D.png");
+   
+
+
+
+}
