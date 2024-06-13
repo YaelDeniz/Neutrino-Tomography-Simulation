1a8143421abab9a016a9e575881f5cdedc433327
Yael Deniz <deni5876@vandals.uidaho.edu>
2024-06-13 14:39:24 -0700

    Neutrino Flux Implementation

--- inc/PhyTools.h
+++ inc/PhyTools.h
@@ -45,8 +45,9 @@ class NuFlux
     bool DataHeader (const std::string &aline); // Get data into tabular form but ignore Headers from file
 
     std::vector<std::vector<double>> reshape(const std::vector<double>& vec, int a, int b); //Reshape colum vector into matrix
-
-    std::vector< std::vector<double> > SetFluxData( std::string nuFluxFile = "./SP_AziAveraged_solmin/spl-nu-20-01-000.d" );
+    
+    // By default we use South Pole Flux with Sol Min
+    std::vector< std::vector<double> > SetFluxData( std::string nuFluxFile = "../NuFlux/SP_AziAveraged_solmn/spl-nu-20-01-000.d" );
     
     TH2D*GetFluxHist(int flavor, std::vector<std::vector<double>> FluxData); // Create 2D Histogram
 
